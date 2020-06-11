3d1b8aedfc0cabfb476984c73e8c0c7b86df80ae
Nigel Bunner <nigelbunner@gmail.com>
2020-06-11 22:29:52 +0100

    Adds buttons etc

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,65 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+// import { Link } from "gatsby"
+import axios from "axios"
+import { styled } from "linaria/react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [disableBtn, setDisableBtn] = useState(false)
+  const btnText = "Publish site"
+  const btnTextUpdating = "Site updating"
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log("here")
+    setDisableBtn(true)
+    // axios
+    //   .post("https://api.netlify.com/build_hooks/5ec6d7e8ce81309f11610c29")
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log(err))
+  }
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>AVM publish page</h1>
+      <h2>Click the button below to start a build of the site</h2>
+      {disableBtn ? (
+        <Btn className="disabled">{btnTextUpdating}</Btn>
+      ) : (
+        <Btn onClick={e => handleSubmit(e)}>{btnText}</Btn>
+      )}
+      <p>
+        This should take upto 5 mins to complete, but shouldn't be no more than
+        3 mins.
+      </p>
+      <h2>The image below shows the current status of the site/build</h2>
+      <img
+        src="https://api.netlify.com/api/v1/badges/f667e17e-f666-46bf-b25f-216f8720eea2/deploy-status"
+        alt="build status"
+      />
+    </Layout>
+  )
+}
+
+const Btn = styled.button`
+  appearance: none;
+  border: 1px solid #007a70;
+  border-radius: 6px;
+  color: #007a70;
+  cursor: pointer;
+  display: block;
+  font-weight: bold;
+  margin: 1rem 0;
+  padding: 12px;
+  text-align: center;
+  width: 200px;
+  &.disabled {
+    border-color: #7a7d82;
+    color: #7a7d82;
+    cursor: default;
+  }
+`
 
 export default IndexPage
