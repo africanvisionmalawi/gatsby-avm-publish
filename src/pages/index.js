c3d8a97dfa5672f49f8251b687e44fbf83b9dc91
Nigel Bunner <nigelbunner@gmail.com>
2021-05-17 22:02:41 +0100

    Adds sanity preview button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react"
-import { styled } from "linaria/react"
 import dayjs from "dayjs"
 import advancedFormat from "dayjs/plugin/advancedFormat"
-
+import { styled } from "linaria/react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -10,11 +9,14 @@ dayjs.extend(advancedFormat)
 
 const IndexPage = () => {
   const [disableBtn, setDisableBtn] = useState(false)
+  const [disablePreviewBtn, setDisabledPreviewBtn] = useState(false)
   const [lastUpdated, setLastUpdated] = useState("")
   const [imageRefresh, setImageRefresh] = useState("")
 
   const btnText = "Publish site"
   const btnTextUpdating = "Site updating"
+  const btnPreviewText = "Restart preview"
+  const btnPreviewTextUpdating = "Preview updating"
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -42,6 +44,26 @@ const IndexPage = () => {
       .catch(error => console.log(error))
   }
 
+  const handlePreviewSubmit = e => {
+    e.preventDefault()
+    // setDisabledPreviewBtn(true)
+    const url = process.env.GATSBY_CLOUD_PREVIEW_BUILD
+
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: "token " + process.env.GATSBY_GITHUB_TOKEN,
+      },
+      body: JSON.stringify({
+        event_type: process.env.GATSBY_EVENT_TYPE,
+      }),
+    }
+    fetch(url, options)
+      .then(res => res.json())
+      .catch(error => console.log(error))
+  }
+
   const handleClick = e => {
     const refreshCount = Math.random()
     setImageRefresh("x" + refreshCount)
@@ -50,30 +72,54 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>AVM publish page</h1>
-      <h2>Click the button below to start a build of the site</h2>
-      {disableBtn ? (
-        <Btn className="disabled">{btnTextUpdating}</Btn>
-      ) : (
-        <Btn onClick={e => handleSubmit(e)}>{btnText}</Btn>
-      )}
-      <p>
-        This should take upto 5 mins to complete, but shouldn't be no more than
-        3 mins.
-      </p>
-      <p>
-        This button will show the current state of the latest build, click on it
-        to refresh the button and show the latest state.
-      </p>
-      <BuildBtn>
-        <img
-          src={`https://api.netlify.com/api/v1/badges/f6e1ff52-323f-4c7f-b2a7-c1980c9d7051/deploy-status?a=${imageRefresh}`}
-          onClick={e => handleClick(e)}
-        />
-      </BuildBtn>
+      <Heading>AVM publish page</Heading>
+      <Columns>
+        <Col>
+          <h2>Click the button below to start a build of the site</h2>
+          {disableBtn ? (
+            <Btn className="disabled">{btnTextUpdating}</Btn>
+          ) : (
+            <Btn onClick={e => handleSubmit(e)}>{btnText}</Btn>
+          )}
+          <p>
+            This should take upto 5 mins to complete, but shouldn't be no more
+            than 3 mins.
+          </p>
+          <p>
+            This button will show the current state of the latest build, click
+            on it to refresh the button and show the latest state.
+          </p>
+          <BuildBtn>
+            <img
+              src={`https://api.netlify.com/api/v1/badges/f6e1ff52-323f-4c7f-b2a7-c1980c9d7051/deploy-status?a=${imageRefresh}`}
+              onClick={e => handleClick(e)}
+            />
+          </BuildBtn>
+        </Col>
+        <Col>
+          <h2>Click the button below to restart the preview server</h2>
+          <p>This should take anything from around 20secs to 1m 30s.</p>
+          {disablePreviewBtn ? (
+            <Btn className="disabled">{btnPreviewTextUpdating}</Btn>
+          ) : (
+            <Btn onClick={e => handlePreviewSubmit(e)}>{btnPreviewText}</Btn>
+          )}
+
+          <p>
+            <a href="https://preview-avmsite21.gtsb.io/" target="_blank">
+              Open preview server
+            </a>{" "}
+            in new window.
+          </p>
+        </Col>
+      </Columns>
     </Layout>
   )
 }
+
+const Heading = styled.h1`
+  text-align: center;
+`
 
 const Btn = styled.button`
   appearance: none;
@@ -98,6 +144,20 @@ const Btn = styled.button`
 const BuildBtn = styled.span`
   cursor: pointer;
   display: block;
+`
+
+const Columns = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+`
+
+const Col = styled.div`
+  padding: 0 1rem;
+  width: 100%;
+  @media (min-width: 768px) {
+    width: 50%;
+  }
 `
 
 export default IndexPage
